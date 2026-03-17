b4a3653377becf25e58c0e0a34d0746cddc643ab
GUSTAVO OLIVEIRA <gugutoktpi2@gmail.com>
2026-03-17 18:47:07 -0300

    Arquivos utilizados ou alterados na aula

--- aula02/app/index.tsx
+++ aula02/app/index.tsx
@@ -1,32 +1,43 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
+//importa a API da câmera
+
 import * as MediaLibrary from "expo-media-library";
+//importa a API da galeria
+
 import { useRef, useState } from "react";
-import { View, Button, Image, StyleSheet } from "react-native";
+import { View, Button, Image, StyleSheet, Alert } from "react-native";
 
 export default function Index() {
 
-  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
-  const [mediaPermission, requestMediaPermission] = MediaLibrary.usePermissions();
-
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions(); //pede permissão da câmera
   const cameraRef = useRef<any>(null);
   const [photo, setPhoto] = useState<any>(null);
 
   async function takePicture() {
     if (cameraRef.current) {
-      const result = await cameraRef.current.takePictureAsync();
+      const result = await cameraRef.current.takePictureAsync(); //Tira a foto
+      
+
       setPhoto(result);
+     
     }
   }
 
   async function savePhoto() {
 
-    if (!mediaPermission?.granted) {
-      await requestMediaPermission();
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    // aqui pede permissão da galeria
+
+    if (status !== "granted") {
+      Alert.alert("Permissão negada");
+      return;
     }
 
     if (photo) {
       await MediaLibrary.saveToLibraryAsync(photo.uri);
-      alert("Foto salva na galeria!");
+      //salva a foto
+
+      Alert.alert("Foto salva na galeria!");
     }
   }
 
@@ -34,23 +45,26 @@ export default function Index() {
     return (
       <View style={styles.center}>
         <Button title="Permitir câmera" onPress={requestCameraPermission} />
+        {/* botão para liberar câmera */}
       </View>
     );
   }
 
   return (
-    <View style={{flex:1}}>
+    <View style={{ flex: 1 }}>
 
       {!photo ? (
         <CameraView
-          style={{flex:1}}
+          style={{ flex: 1 }}
           ref={cameraRef}
         />
+        // mostra a câmera
       ) : (
         <Image
-          source={{uri:photo.uri}}
-          style={{flex:1}}
+          source={{ uri: photo.uri }}
+          style={{ flex: 1 }}
         />
+        // mostra a foto
       )}
 
       <View style={styles.buttons}>
@@ -72,17 +86,19 @@ export default function Index() {
 
 const styles = StyleSheet.create({
 
-  center:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+   
   },
 
-  buttons:{
-    position:"absolute",
-    bottom:80,
-    alignSelf:"center",
-    gap:10
+  buttons: {
+    position: "absolute",
+    bottom: 80,
+    alignSelf: "center",
+    gap: 10
+   
   }
 
 });
